2838b7b92f99af16ac5be55f2246d1a6d491e62c
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2017-10-26 12:30:01 +0530

    Remove instances of %m
    It's not ISO C. It would have been better if 'fgets' didn't add
    the line terminator while reading input as it's more secure in other
    ways.
    
    Unfortunately, I'm left with using plain scanf (which is not so secure)
    as 'fgets' adds the line terminator to input; until I find a better way
    that's more useful. (Fortunately, this is not going to be used by anyone
    in the wild, I hope !!)

--- sister_lab/find_and_replace_test.c
+++ sister_lab/find_and_replace_test.c
@@ -3,26 +3,25 @@
 #include "string/find_and_replace.h"
 
 #define MAX_INPUT_SIZE (100+1)
-//#define MAX_SEARCH_TERM_SIZE (15+1)
-//#define MAX_REPLACE_TERM_SIZE (15+1)
+#define MAX_SEARCH_TERM_SIZE (15+1)
+#define MAX_REPLACE_TERM_SIZE (15+1)
 
 int main(void) {
 	char *input_str = malloc(MAX_INPUT_SIZE), *result = malloc(MAX_INPUT_SIZE);
-	char *search;// = malloc(MAX_SEARCH_TERM_SIZE);
-	char *replace;// = malloc(MAX_REPLACE_TERM_SIZE);
+	char *search = malloc(MAX_SEARCH_TERM_SIZE);
+	char *replace = malloc(MAX_REPLACE_TERM_SIZE);
 
 	// get the inputs
 	printf("Enter the string: ");
 	fgets(input_str, MAX_INPUT_SIZE, stdin);
 
 	printf("Enter search string: ");
-	scanf("%ms", &search);
+	scanf("%s", search);
+	// fgets(replace, MAX_REPLACE_TERM_SIZE, stdin);
 
 	printf("Enter replace string: ");
-	scanf("%ms", &replace);	
-//	fgets(replace, MAX_REPLACE_TERM_SIZE, stdin);
-
-//	printf("%s %zu %s", input_str, strlen(search), replace);
+	scanf("%s", replace);	
+	// fgets(replace, MAX_REPLACE_TERM_SIZE, stdin);
 
 	// do the replacement
 	replace_all(input_str, result, search, replace);

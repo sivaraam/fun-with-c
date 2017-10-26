497f00ecffcebb3fd6dc3c5425227cc3c0d19969
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2017-10-26 09:07:14 +0530

    Add replace_end implementation to string library
    This function should replace the last instance of a word with
    the given replacement.
    
    Also, add a test implementation for trying out replace_end. (Should
    make it a "test" that asserts that it does the right thing!)

--- sister_lab/string/find_and_replace.c
+++ sister_lab/string/find_and_replace.c
@@ -1,6 +1,9 @@
 #include <stddef.h>
 #include <string.h>
 
+// find the occurence of 'search_string' in 'str' and return the offset at which
+// it is present; else return -1
+// So, return is value is either -1 (or) [0, str_len)
 static ptrdiff_t find(char *str, size_t str_len, char *search_string, size_t search_len) {
 	size_t str_pos = 0;
 
@@ -13,8 +16,6 @@ static ptrdiff_t find(char *str, size_t str_len, char *search_string, size_t sea
 	return -1;
 }
 
-// Replace all occurences of 'search' in 'str' by 'replace' and return
-// resulting string in 'result'
 void replace_all(char *input_str, char *result, char *search, char *replace) {
 	size_t input_len = 0, search_len = 0, replace_len = 0;
 	size_t to_copy_start = 0, result_marker = 0;
@@ -32,6 +33,42 @@ void replace_all(char *input_str, char *result, char *search, char *replace) {
 		ptrdiff_t offset = find(input_str+to_copy_start, input_len-to_copy_start, search, search_len);
 
 		if (offset != -1) {
+
+			// copy the string between the last copied position and currently found position
+			strncpy(result+result_marker, input_str+to_copy_start, offset);
+			result_marker += offset;
+
+			// copy the found instance with the replace word
+			strncpy(result+result_marker, replace, replace_len);
+			result_marker += replace_len;
+			to_copy_start += offset+search_len;
+
+		} else {
+			strncpy(result+result_marker, input_str+to_copy_start, input_len-to_copy_start);
+			break;
+		}
+
+	}
+}
+
+void replace_end(char *input_str, char *result, char *search, char *replace) {
+	size_t input_len = 0, search_len = 0, replace_len = 0;
+	size_t to_copy_start = 0, result_marker = 0;
+
+	// get the sizes
+	input_len = strlen(input_str);
+	search_len = strlen(search);
+	replace_len = strlen(replace);
+
+	// start searching and replacing!
+	while (1) {
+
+		// Get the offset at which search term is present in unanalyzed portion of input_str.
+		// i.e., offset of search term in string starting at 'input_str+to_copy_start'.
+		ptrdiff_t offset = find(input_str+to_copy_start, input_len-to_copy_start, search, search_len);
+		_Bool is_last = (input_len-to_copy_start-offset) == search_len;
+
+		if (offset != -1 && is_last) {
 
 			// copy the string between the last copied position and currently found position
 			strncpy(result+result_marker, input_str+to_copy_start, offset);
--- sister_lab/string/find_and_replace.h
+++ sister_lab/string/find_and_replace.h
@@ -1,6 +1,13 @@
 #ifndef FIND_REPLACE
 #define FIND_REPLACE
 
+// Replace all occurences of 'search' in 'str' by 'replace' and return
+// resulting string in 'result'
 extern void replace_all(char *input, char *result, char *search, char *replace);
+
+
+// Replace occurence of 'search' that appears at the end of 'str'
+// by 'replace' and return resulting string in 'result'
+extern void replace_end(char *input, char *result, char *search, char *replace);
 
 #endif

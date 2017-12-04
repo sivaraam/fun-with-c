8ce30b0608383cf6f034e7ede9202d8cc53f668c
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2017-12-04 11:19:15 +0530

    array_parameter: revamp the playful implementation
    This is done to test more things and to see their outcomes.

--- array_parameter.c
+++ array_parameter.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 void foo(int a[10]) {
+    printf("Address of argument: %p", &a);
     printf("%ld\n", sizeof(a));
-    for (int i=0; i<10; i++)
-        printf("%d\t", a[i]);
+    for (int i=0; i<15; i++)
+        printf("(address, value): (%p, %d)\n", (a+i), *(a+i));
     printf("\n");
 }
 
@@ -18,10 +19,21 @@ int main(void) {
     int a[15] = {15,7,0,9};
     int *b = malloc(sizeof(int)*15);
     int c[4] = {1, 2, 3, 4};
+    int d;
 
     fill_array(b, a, 4u);
 
+    printf("(address, value): (%p, %d)", &d, d);
+
+    printf("sizeof(a): %zu\n", sizeof(a));
+    printf("sizeof(b): %zu\n", sizeof(b));
+    printf("sizeof(c): %zu\n", sizeof(c));
+
     foo(a);
     foo(b);
+    foo(c);
+
+    a[7] = 100;
+    foo(a);
     foo(c);
 }

66d2e8275eb99eff44749e9633fe27611a65be01
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-02-13 19:55:04 +0530

    intern/test: add tests
    - test about assigning constant string literal to pointer
    - test about giving value(instead of address) to scanf

--- intern/test.c
+++ intern/test.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
 	printf("size_t: %zu short: %zu char: %zu\n", sizeof(size_t), sizeof(short), sizeof(char));
+
+	struct st
+	{
+		char *name;
+	};
+
+	struct st obj;
+	obj.name = "Hello";
+	obj.name = malloc(sizeof(char)*20);
+	sscanf("Hello", "%s", obj.name);
+
+	printf("%s", obj.name);
+//	*name = "Hello"
+
+	int a;
+	scanf("%d", a);
 }

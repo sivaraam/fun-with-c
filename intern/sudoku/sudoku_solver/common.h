b12e2b6c58b169ebcb809af71264daf177450402
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-02-15 12:44:10 +0530

    intern/sudoku_solver: macro guard the header

--- intern/sudoku/sudoku_solver/common.h
+++ intern/sudoku/sudoku_solver/common.h
@@ -1,3 +1,8 @@
+#ifndef KS_SUDOKE_SOLVER_COMMON
+#define KS_SUDOKE_SOLVER_COMMON
+
 #include "sudoku_solver.h"
 
 void print_table(unsigned sudoku_table[TABLE_ORDER_MAX][TABLE_ORDER_MAX]);
+
+#endif

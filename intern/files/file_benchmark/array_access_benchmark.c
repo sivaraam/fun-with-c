02b747dce5d5c8fac0bb5e32c16febbe7325c9e2
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-02-20 12:23:05 +0530

    intern/files: revamp benchmark
    Benchmark by randomly accessing the different lines to see what
    happens. This randomness might help in identifying the difference
    between file access and normal access.

--- intern/files/file_benchmark/array_access_benchmark.c
+++ intern/files/file_benchmark/array_access_benchmark.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define LINES 100000
 
 extern char find[][1000];
 
 int main(void)
 {
-  for (size_t i=0; i<100000;i++)
+  for (size_t i=0; i<LINES;i++)
   {
-    printf("%s\n", find[i]); //fwrite(find[i], 100, 1, stdout);
+    size_t index = rand()%LINES;
+    printf("%s\n", find[index]); //fwrite(find[i], 100, 1, stdout);
+  }
+
+  printf("\n");
+  clock_t c = clock();
+  if (c == (clock_t)-1)
+  {
+      printf("Could not get clock!\n");
+  }
+  else
+  {
+    printf("CPU time: %lf\n", (float)c/CLOCKS_PER_SEC);
   }
 }
--- intern/files/file_benchmark/file_access_benchmark.c
+++ intern/files/file_benchmark/file_access_benchmark.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../file_lines_offset/file_lines_offset.h"
+
+#define LINES 100000
+#define FILE_SIZE_BYTES 3915074
 
 int main(void)
 {
-  char buf[100+1];
-  FILE *fp = fopen("find_op", "r");
+  char *file_name = "find_op";
+  char buf[10000+1];
+  FILE *fp = fopen(file_name, "r");
   unsigned long round = 1;
 
-  while(!feof(fp))
+  struct lines *l = get_line_offsets(file_name);
+
+  for (unsigned i=0; i<l->lines; i++)
   {
-    fprintf(stderr, "Successfully read '%zu' item in round: %lu\n", fread(buf, 100, 1, fp), round);
-    fwrite(buf, 100, 1, stdout);
-//    printf("%s", buf);
-    fflush(stdout);
+    unsigned long random_line_byte = *(l->byte_offsets + (rand()%l->lines));
+
+    fseek(fp, random_line_byte, SEEK_SET);
+    fscanf(fp, "%[^\n]s", buf);
+    // read the new line
+    fgetc(fp);
+
+    printf("%s\n", buf);
     round++;
+  }
+
+  printf("\n");
+  clock_t c = clock();
+  if (c == (clock_t)-1)
+  {
+      printf("Could not get clock!\n");
+  }
+  else
+  {
+    printf("clock: %lf\n", (float)c/CLOCKS_PER_SEC);
   }
 }

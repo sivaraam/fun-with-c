02541afb75b7bdca663ae65a92b747375eb49a58
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-02-25 08:52:27 +0530

    intern/maze: allow for removing adjacencies from the adjacency list
    This would allow for pruning the dead end nodes and thus help in
    reducing the search space.
    
    Double pointer ocunter part of 23a8db9 (intern/maze: allow for removing
    adjacencies from the adjacency list, 2018-02-25)

--- intern/files/maze/graph/maze_graph.c
+++ intern/files/maze/graph/maze_graph.c
@@ -29,3 +29,36 @@ int insert_adjacency(struct node *const n, struct node *const adj)
 
 	return 0;
 }
+
+int remove_adjacency(struct node *const n, struct node *const adj)
+{
+	if (n == NULL ||
+	    adj == NULL)
+	{
+		return ERRNULL;
+	}
+
+	for (unsigned curr_adj_index=0; curr_adj_index<n->adjlist.num; curr_adj_index++)
+	{
+		if (*(n->adjlist.adjs + curr_adj_index) == adj)
+		{
+			for (size_t chunk=n->adjlist.num-1; chunk>curr_adj_index; chunk--)
+			{
+				*(n->adjlist.adjs + chunk - 1) = *(n->adjlist.adjs + chunk);
+			}
+			n->adjlist.num--;
+			if (n->adjlist.num%2 == 0)
+			{
+				struct node **temp = realloc(n->adjlist.adjs, (n->adjlist.num)*sizeof(struct node *));
+				if (temp == NULL)
+				{
+					return ERRMEMORY;
+				}
+				n->adjlist.adjs = temp;
+			}
+			return 0;
+		}
+	}
+
+	return ERRNOADJ;
+}

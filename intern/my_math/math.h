a77db5671636444baa71cf026c02f884e71dab45
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-02-27 18:43:41 +0530

    intern/math: add a simple implementation for the integral absolute function

--- intern/my_math/math.h
+++ intern/my_math/math.h
@@ -2,6 +2,11 @@
 #define KS_MY_MATH
 
 /**
+ * Returns the absolute value for the given integer.
+ */
+int math_abs(int val);
+
+/**
  * Return the largest integral value not greater than m.
  */
 double math_floor(double m);

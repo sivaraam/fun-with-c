4253b048088c0b11519c86146b4690a8db762d44
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-03-05 11:22:28 +0530

    intern/maze: shrink the image data before solving the maze
    This helps in cutting down the memory requirements a little while also
    simplifying the access of pixels in the image data.

--- intern/files/maze/maze_solver_helpers.c
+++ intern/files/maze/maze_solver_helpers.c
@@ -27,7 +27,7 @@ int is_hurdle_pixel(struct maze_image *const maze, unsigned pixel)
 	}
 #endif
 
-	const unsigned char *const pixel_byte = maze->data + get_pixel_offset(maze->width, maze->padding, pixel);
+	const unsigned char *const pixel_byte = maze->data + pixel;
 
 	// It's enough to check one byte for now
 	return ((*pixel_byte&HURDLE_PIXEL) == (*pixel_byte)) ? 1 :  0;
@@ -46,7 +46,7 @@ int is_clear_pixel(struct maze_image *const maze, unsigned pixel)
 	}
 #endif
 
-	const unsigned char *const pixel_byte = maze->data + get_pixel_offset(maze->width, maze->padding, pixel);
+	const unsigned char *const pixel_byte = maze->data + pixel;
 
 	// It's enough to check one byte for now
 	return ((*pixel_byte&CLEAR_PIXEL) == CLEAR_PIXEL) ? 1 : 0;
@@ -70,7 +70,7 @@ long find_gate(struct maze_image *const maze, unsigned start_pixel, unsigned end
 	// find the pixel of the start gate
 	for (unsigned pixel = start_pixel; pixel<=end_pixel; pixel++)
 	{
-		unsigned char *const pixel_ptr = maze->data+get_pixel_offset(maze->width, maze->padding, pixel);
+		unsigned char *const pixel_ptr = maze->data + pixel;
 
 		if ((*pixel_ptr&CLEAR_PIXEL) == CLEAR_PIXEL)
 		{
@@ -549,19 +549,11 @@ void delete_graph(struct maze_image *maze)
 inline static
 void colour_pixel(struct maze_image *const maze, unsigned pixel)
 {
-	unsigned char *const pixel_bytes = maze->data + get_pixel_offset(maze->width, maze->padding, pixel);
+	unsigned char *const pixel_byte = maze->data + pixel;
 
-	// ordering of RGB for little-endian architecture
-	static const unsigned char colour_bytes[3] = {
-	   0x00, // blue,
-	   0x00, // green
-	   0xFF //red
-	};
+	static const unsigned char colour_byte = 0x77;
 
-	for (unsigned byte=0; byte<PIXEL_BYTES; byte++)
-	{
-		*(pixel_bytes + byte) = colour_bytes[byte];
-	}
+	*(pixel_byte) = colour_byte;
 }
 
 void colour_path(struct maze_image *const maze, struct sp_queue_head *const sp)

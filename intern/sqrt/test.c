8003a63b84f84bc556330ae0751e3f7418cc3bf0
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-03-12 00:30:40 +0530

    intern/sqrt: perform input validation

--- intern/sqrt/test.c
+++ intern/sqrt/test.c
@@ -5,8 +5,30 @@
 
 int main(void) {
 	double user_input;
-	printf("Enter a non-negative value: ");
-	scanf("%lf", &user_input);
+	int scanf_ret;
+
+	do
+	{
+		printf("Enter a non-negative value: ");
+		scanf_ret = scanf("%lf", &user_input);
+
+		if (scanf_ret == 0)
+		{
+			// discard the inputs up and until the first newline
+			// (the typical range of invalid inputs)
+			int ch = EOF;
+			
+			do
+			{
+				ch = getchar();
+				if (ch == EOF)
+				{
+					return 1;
+				}
+			} while (ch != '\n');
+		}
+	} while (scanf_ret == 0);
+
 	double sqrt_res = my_sqrt(user_input);
 	if (sqrt_res != MY_EINVALID_INPUT) {
 		printf("sqrt(%.3f): %.3f\n", user_input, sqrt_res);

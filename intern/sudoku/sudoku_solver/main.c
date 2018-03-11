a4041661f332ecbac9a48c38be6239134dd3ab74
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-03-12 00:18:34 +0530

    intern/sudoku_solver: validate input and discard invalid inputs

--- intern/sudoku/sudoku_solver/main.c
+++ intern/sudoku/sudoku_solver/main.c
@@ -14,15 +14,35 @@ void print_welcome_message(void)
 	printf("\t- A cell only has valid entries 0-9, where 0 represents an empty cell\n");
 }
 
-void get_table(unsigned int table[TABLE_ORDER_MAX][TABLE_ORDER_MAX])
+/**
+ * Returns zero if the read was successful else returns a non-zer valud in case of an error.
+ */
+int get_table(unsigned int table[TABLE_ORDER_MAX][TABLE_ORDER_MAX])
 {
+	printf ("Only enter valid inputs (0-9). Invalid inputs are discarded.\n");
 	for (size_t row=0; row<TABLE_ORDER_MAX; row++)
 	{
 		for (size_t col=0; col<TABLE_ORDER_MAX; col++)
 		{
-			scanf("%u", &table[row][col]);
+			printf ("Enter the value for cell at row: %zu, col: %zu: ", row+1, col+1);
+			int input = EOF;
+			do {
+				input = getchar();
+				if (input == EOF)
+				{
+					printf ("Could not sccessfully read the input!\n");
+					return 1;
+				}
+				if (input >= '0' && input <= '9')
+				{
+					table[row][col] = input - '0';
+					break;
+				}
+			} while (1);
 		}
 	}
+
+	return 0;
 }
 
 void print_solution(unsigned int table[TABLE_ORDER_MAX][TABLE_ORDER_MAX])
@@ -36,7 +56,10 @@ int main(void)
 {
 	unsigned table[TABLE_ORDER_MAX][TABLE_ORDER_MAX] = {0};
 	print_welcome_message();
-	get_table(table);
+	if (get_table(table))
+	{
+		return 1;
+	}
 	solve_sudoku(table);
 	print_solution(table);
 }
